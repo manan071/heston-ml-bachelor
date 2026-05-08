73746b6d052bcfe22d74a2abba2f88284a79ae09
Manan Hadayat <manan.nasrullah@gmail.com>
2026-05-08 19:51:32 +0200

    Minor changes

--- heston_NN_functions.py
+++ heston_NN_functions.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def nn_price(S, K, tau, kappa, theta, sigma, rho, v0, r, q, scaler_X, scaler_y, model, device):
     """Calculate the option price using the trained neural network model."""
-    
+
     x  = scaler_X.transform([[np.log(K/S), tau, kappa, theta, sigma, rho, v0, r, q]])
 
     with torch.no_grad():
@@ -13,4 +13,3 @@ def nn_price(S, K, tau, kappa, theta, sigma, rho, v0, r, q, scaler_X, scaler_y, 
     price = scaler_y.inverse_transform(nn_price)[0][0] * S
 
     return price
-
