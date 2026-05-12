725bdda610969539e0f7abd2fb758dba2c64332e
Manan Hadayat <manan.nasrullah@gmail.com>
2026-05-12 15:44:36 +0200

    Added IV curve for NN

--- NN_results.py
+++ NN_results.py
@@ -41,23 +41,8 @@ utils.plot_loss('3-training_log.csv')
 utils.plot_loss('4-training_log.csv')
 utils.plot_loss('5-training_log.csv')
 """
-
-# Plot NN prices vs FFT prices
-strikes_fft, prices_fft = heston_call_FFT(4096, 0.25, 1.5, S, tau, kappa, theta, 
-                                          sigma, rho, v0, r, q, trap=1) 
-
-# Filter 
-mask = (strikes_fft >= 30) & (strikes_fft <= 130) & (prices_fft > 0)
-strikes_fft = strikes_fft[mask]
-prices_fft = prices_fft[mask]
-
-prices_NN = np.array([model(torch.tensor(scaler_X.transform([[np.log(K/S), tau, kappa, theta, sigma, rho, v0, r, q]]), 
-                                         dtype=torch.float32).to(device)).detach().cpu().numpy()[0][0] for K in strikes_fft])
-
-prices_NN = scaler_y.inverse_transform(prices_NN.reshape(-1, 1)).flatten()*S
-
-utils.plot_prices_vs_strike('NN', prices_NN, prices_fft, strikes_fft)
-
+"""
+# Plot NN prices vs FFT prices and print error metrics
 parameter_sets = [
 
         {'name': 'Moderate benchmark case', 'tau': 0.5, 'kappa': 2.0, 'theta': 0.04, 'sigma': 0.3, 'rho': -0.7, 'v0': 0.04, 
@@ -96,50 +81,104 @@ for param_set in parameter_sets:
     utils.plot_prices_vs_strike('NN', prices_NN, prices_fft, strikes_fft)
     utils.plot_predicted_vs_benchmark('NN', prices_NN, prices_fft)
 
+    # Error metrics
+    print(f"Error metrics for {param_set['name']}:")
 
-# Error metrics
-atm_band = 0.01
-# OTM options
-mask_otm = strikes_fft > S*(1+atm_band)
-mae, mre, mse, rmse, median_error, min_error, max_error = utils.error_metrics(prices_NN[mask_otm], prices_fft[mask_otm], S)
+    atm_band = 0.01
 
-print(f"\n-------------------------------")
-print(f"OTM options:")
-print(f"-------------------------------")
-print("MAE:", mae)
-print("MRE:", mre)
-print("MSE:", mse)
-print("RMSE:", rmse)
-print("Median Error:", median_error)
-print("Min Error:", min_error)
-print("Max Error:", max_error)
+    # OTM options
+    mask_otm = strikes_fft > S*(1+atm_band)
+    mae, mre, mse, rmse, median_error, min_error, max_error = utils.error_metrics(prices_NN[mask_otm], prices_fft[mask_otm], S)
 
-# Near ATM options
-mask_atm = (strikes_fft >= S*(1-atm_band)) & (strikes_fft <= S*(1+atm_band))
-mae, mre, mse, rmse, median_error, min_error, max_error = utils.error_metrics(prices_NN[mask_atm], prices_fft[mask_atm], S)
+    print(f"\n-------------------------------")
+    print(f"OTM options:")
+    print(f"-------------------------------")
+    print("MAE:", mae)
+    print("MRE:", mre)
+    print("MSE:", mse)
+    print("RMSE:", rmse)
+    print("Median Error:", median_error)
+    print("Min Error:", min_error)
+    print("Max Error:", max_error)
 
-print(f"\n-------------------------------")
-print(f"Near ATM options:")
-print(f"-------------------------------")
-print("MAE:", mae)
-print("MRE:", mre)
-print("MSE:", mse)
-print("RMSE:", rmse)
-print("Median Error:", median_error)
-print("Min Error:", min_error)
-print("Max Error:", max_error)
+    # Near ATM options
+    mask_atm = (strikes_fft >= S*(1-atm_band)) & (strikes_fft <= S*(1+atm_band))
+    mae, mre, mse, rmse, median_error, min_error, max_error = utils.error_metrics(prices_NN[mask_atm], prices_fft[mask_atm], S)
 
-# ITM options
-mask_itm = strikes_fft < S*(1-atm_band)
-mae, mre, mse, rmse, median_error, min_error, max_error = utils.error_metrics(prices_NN[mask_itm], prices_fft[mask_itm], S)
+    print(f"\n-------------------------------")
+    print(f"Near ATM options:")
+    print(f"-------------------------------")
+    print("MAE:", mae)
+    print("MRE:", mre)
+    print("MSE:", mse)
+    print("RMSE:", rmse)
+    print("Median Error:", median_error)
+    print("Min Error:", min_error)
+    print("Max Error:", max_error)
 
-print(f"\n-------------------------------")
-print(f"ITM options:")
-print(f"-------------------------------")
-print("MAE:", mae)
-print("MRE:", mre)
-print("MSE:", mse)
-print("RMSE:", rmse)
-print("Median Error:", median_error)
-print("Min Error:", min_error)
-print("Max Error:", max_error)
+    # ITM options
+    mask_itm = strikes_fft < S*(1-atm_band)
+    mae, mre, mse, rmse, median_error, min_error, max_error = utils.error_metrics(prices_NN[mask_itm], prices_fft[mask_itm], S)
+
+    print(f"\n-------------------------------")
+    print(f"ITM options:")
+    print(f"-------------------------------")
+    print("MAE:", mae)
+    print("MRE:", mre)
+    print("MSE:", mse)
+    print("RMSE:", rmse)
+    print("Median Error:", median_error)
+    print("Min Error:", min_error)
+    print("Max Error:", max_error)
+"""
+
+# Plot volatility smiles for the NN predictions using pricing NN
+strikes_list = []
+ivs_list = []
+labels = []
+
+# Rho
+for rho_2 in [-0.8, -0.4, 0.0]:
+    strikes, prices = heston_call_FFT(4096, 0.25, 1.5, S, tau, kappa, theta, 
+                                      sigma, rho_2, v0, r, q, trap=1)
+    
+    prices_NN = np.array([model(torch.tensor(scaler_X.transform([[np.log(K/S), tau, kappa, theta, sigma, rho_2, v0, r, q]]), 
+                                         dtype=torch.float32).to(device)).detach().cpu().numpy()[0][0] for K in strikes])
+    
+    prices_NN = scaler_y.inverse_transform(prices_NN.reshape(-1, 1)).flatten()*S 
+
+    mask = (strikes >= 30) & (strikes <= 130) & (prices > 0)
+    strikes = strikes[mask]
+    prices_NN = prices_NN[mask]
+
+    ivs = utils.implied_volatility_vectorized(prices_NN, strikes, S, tau, r, q)
+    strikes_list.append(strikes)
+    ivs_list.append(ivs)
+    labels.append(rf'$\rho={rho_2}$')
+
+utils.plot_volatility_smile(strikes_list, ivs_list, labels)
+
+# Sigma
+strikes_list.clear()
+ivs_list.clear()
+labels.clear()
+
+for sigma in [0.1, 0.3, 0.6]:
+    strikes, prices = heston_call_FFT(4096, 0.25, 1.5, S, tau, kappa, theta, 
+                                      sigma, rho, v0, r, q, trap=1)
+    
+    prices_NN = np.array([model(torch.tensor(scaler_X.transform([[np.log(K/S), tau, kappa, theta, sigma, rho, v0, r, q]]), 
+                                         dtype=torch.float32).to(device)).detach().cpu().numpy()[0][0] for K in strikes])
+    
+    prices_NN = scaler_y.inverse_transform(prices_NN.reshape(-1, 1)).flatten()*S 
+
+    mask = (strikes >= 30) & (strikes <= 130) & (prices_NN > 0)
+    strikes = strikes[mask]
+    prices_NN = prices_NN[mask]
+
+    ivs = utils.implied_volatility_vectorized(prices_NN, strikes, S, tau, r, q)
+    strikes_list.append(strikes)
+    ivs_list.append(ivs)
+    labels.append(rf'$\sigma={sigma}$')
+
+utils.plot_volatility_smile(strikes_list, ivs_list, labels)
